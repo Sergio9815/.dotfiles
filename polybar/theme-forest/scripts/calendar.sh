b5df30a790be77f1536a1681845c87f0491fd477
Sergio9815 <gsergio1598@gmail.com>
2021-09-09 02:31:14 -0500

    :sparkles: add next, previous and complete calendar module

--- polybar/theme-forest/scripts/calendar.sh
+++ polybar/theme-forest/scripts/calendar.sh
@@ -1,6 +1,39 @@
 #!/usr/bin/env bash
 
+## ================================
+##    Author  : Sergio Gonzalez
+##    Github  : @Sergio9815
+## ================================
+
+## Dependencies
+##    - cal / ncal
+##    - rofi
+
 dir="~/.config/polybar/forest/scripts/rofi"
 
-DATE="$(date +%d)"
-ncal -b -h | rofi -theme $dir/calendar.rasi -dmenu -p "My Calendar | Day: $DATE |"
+TODAY="$(date +%d)"
+MONTH="$(date +%-m)"
+YEAR="$(date +%Y)"
+
+current() {
+  ncal -b -h | rofi -theme $dir/calendar.rasi -dmenu -p "My Calendar | Day: $TODAY |"
+}
+
+next() {
+  ncal -b -h -m "$(($MONTH + 1))" | rofi -theme $dir/calendar.rasi -dmenu -p "My Calendar | Next  |"
+}
+
+previous() {
+  ncal -b -h -m "$(($MONTH - 1))" | rofi -theme $dir/calendar.rasi -dmenu -p "My Calendar | Prev  |"
+}
+
+all() {
+  ncal -y -b -h | rofi -theme $dir/calendarAll.rasi -dmenu -p "My Calendar | Year: $YEAR"
+}
+
+case $1 in
+	"curr") current;;
+  "next") next;;
+  "prev") previous;;
+  "all") all;;
+esac
