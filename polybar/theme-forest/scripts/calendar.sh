d244d2ea26503942e265f6a8ce1c5e888230e9d5
Sergio9815 <gsergio1598@gmail.com>
2021-09-09 15:23:45 -0500

    :sparkles: update calendar script

--- polybar/theme-forest/scripts/calendar.sh
+++ polybar/theme-forest/scripts/calendar.sh
@@ -8,27 +8,35 @@
 ## Dependencies
 ##    - cal / ncal
 ##    - rofi
+##    - nerd fonts: CaskaydiaCove, Meslo, etc..
 
 dir="~/.config/polybar/forest/scripts/rofi"
 
 TODAY="$(date +%d)"
 MONTH="$(date +%-m)"
-YEAR="$(date +%Y)"
+
+uppercase() {
+  string="$(date "+%a, %b %d %Y")"
+  first=`echo $string|cut -c1|tr [a-z] [A-Z]`
+  second=`echo $string|cut -c2-`
+  DATE=$first$second
+}
 
 current() {
-  ncal -b -h | rofi -theme $dir/calendar.rasi -dmenu -p "My Calendar | Day: $TODAY |"
+  ncal -b -h | rofi -theme $dir/calendar.rasi -dmenu -p "My Calendar ﲯﲯ Day $TODAY"
 }
 
 next() {
-  ncal -b -h -m "$(($MONTH + 1))" | rofi -theme $dir/calendar.rasi -dmenu -p "My Calendar | Next  |"
+  ncal -b -h -m "$(($MONTH + 1))" | rofi -theme $dir/calendar.rasi -dmenu -p "My Calendar ﲯﲯ Next "
 }
 
 previous() {
-  ncal -b -h -m "$(($MONTH - 1))" | rofi -theme $dir/calendar.rasi -dmenu -p "My Calendar | Prev  |"
+  ncal -b -h -m "$(($MONTH - 1))" | rofi -theme $dir/calendar.rasi -dmenu -p "My Calendar ﲯﲯ Prev "
 }
 
 all() {
-  ncal -y -b -h | rofi -theme $dir/calendarAll.rasi -dmenu -p "My Calendar | Year: $YEAR"
+  uppercase
+  ncal -y -b -h | rofi -theme $dir/calendarAll.rasi -dmenu -p "My Calendar ﲯﲯ $DATE   "
 }
 
 case $1 in
