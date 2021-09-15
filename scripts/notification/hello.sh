ed8f40b2fe6a12bb497ce3e2053091e664a95ab3
Sergio9815 <gsergio1598@gmail.com>
2021-09-14 22:12:09 -0500

    :art: some configs

--- scripts/notification/hello.sh
+++ scripts/notification/hello.sh
@@ -16,13 +16,13 @@ datey=$(date +%H)
 
 if [ "$datey" -ge 06 ] && [ "$datey" -le 11 ] 
 then
-    dunstify -i ~/.config/dunst/icons/squirtle.png "Good Morning" "$NAME!" -u low
+    dunstify -i ~/.config/dunst/icons/charmander.svg "Good Morning" "$NAME!" -u low
 elif [ "$datey" -ge 12 ] && [ "$datey" -le 17 ] 
 then
     dunstify -i ~/.config/dunst/icons/pikachu.svg "Good Afternoon" "$NAME!" -u low
 elif [ "$datey" -ge 18 ] && [ "$datey" -le 23 ] 
 then
-    dunstify -i ~/.config/dunst/icons/charmander.svg "Good Evening" "$NAME!" -u low
+    dunstify -i ~/.config/dunst/icons/squirtle.svg "Good Evening" "$NAME!" -u low
 else 
-    dunstify -i ~/.config/dunst/icons/reaper.png "Demons are watching you..." -u low
+    dunstify -i ~/.config/dunst/icons/venonat.svg "Demons are watching you..." -u low
 fi
