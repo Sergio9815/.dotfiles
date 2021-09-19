12a1dbb204f30ac5c88ce95fa26d74155c3bdb32
Sergio9815 <gsergio1598@gmail.com>
2021-09-18 22:27:24 -0500

    :sparkles: add keyboard notification

---
+++ scripts/notification/layout.sh
@@ -0,0 +1,28 @@
+#!usr/bin/sh
+
+welcome() {
+  printf "%s" "\
+
+███████  ███████           ███   ███   ███████
+██     ██                ███   ███   ██
+███████  ███████  ███████  ███   ███   ███████
+██          ██           ███   ███         ██ 
+███████  ███████            ████████    ███████ 
+                                                                                   
+"
+exit 1
+}
+
+APPNAME="Es"
+
+layout="$(xkblayout-state print "%s")"
+
+echo $layout
+
+if [ $layout = "us" ]
+then
+    dunstify -u low -h string:fgcolor:#C1B3FF '      ' -i ~/.config/dunst/icons/spell_book.svg   "<span foreground='#ebb9b9' font_desc='Cartograph CF Italic 19'><b>~</b></span><span foreground='#ebe3b9' font_desc='Cartograph CF Italic 19'><b> US </b></span><span foreground='#caf6bb' font_desc='Cartograph CF Italic 19'><b>~</b></span> \n<span foreground='#cddbf9' font_desc='Cartograph CF 13'><b>Keyboard layout!</b></span>"
+elif [ $layout = "es" ]
+then
+    dunstify -u low -h string:fgcolor:#C1B3FF '      ' -i ~/.config/dunst/icons/spell_book.svg   "<span foreground='#ebb9b9' font_desc='Cartograph CF Italic 19'><b>~</b></span><span foreground='#ebe3b9' font_desc='Cartograph CF Italic 19'><b> ES </b></span><span foreground='#caf6bb' font_desc='Cartograph CF Italic 19'><b>~</b></span> \n<span foreground='#cddbf9' font_desc='Cartograph CF 13'><b>Keyboard layout!</b></span>"
+fi
